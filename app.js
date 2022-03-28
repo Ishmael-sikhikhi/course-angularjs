333c37796409df52dd0b86b36988f92f28a16b54
Ishmael <70352247+Ishmael-sikhikhi@users.noreply.github.com>
2022-03-28 13:56:50 +0200

    Use Angular

--- app.js
+++ app.js
@@ -2,7 +2,20 @@
     'use strict';
     
     angular.module('myFirstApp' , [])
-    .controller('MyFirstController' , function(){
-
+    .controller('MyFirstController' , function($scope){
+        $scope.name = ""
+        $scope.totalValue = 0;
+        $scope.displayNumeric = function (){
+            var totalNameValue = calculateNumericForString($scope.name);
+            $scope.totalValue = totalNameValue;
+        };
     });
+
+    function calculateNumericForString(string) {
+        var totalNameValue = 0;
+        for (var i = 0; i < string.length; i++) {
+            totalNameValue += string.charCodeAt(i);
+        }
+        return totalNameValue;
+    }
 })();
